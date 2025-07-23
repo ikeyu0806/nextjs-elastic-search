3e04445d79be64e14dc5a59772fab82f842799c0
ikeyu0806 <ikeyu0806@gmail.com>
2025-07-23 17:15:10 +0900

    run prettier, delete comment

--- README.md
+++ README.md
@@ -10,11 +10,13 @@ docker compose up
 ```
 
 seed登録
+
 ```
 npx ts-node scripts/seed.ts
 ```
 
 index内容の確認
+
 ```
 npx ts-node scripts/fatchRestrant.ts
 ```
--- app/page.tsx
+++ app/page.tsx
@@ -54,12 +54,6 @@ export default function Home() {
         </div>
       </div>
       <div className="col-span-8 ml-4 mt-8">
-        {/* <Card href="#" className="h-full">
-          <h5 className="text-2xl font-bold tracking-tight">スタミナ苑</h5>
-          <p className="font-normal">
-            予約不可！行列必至！昭和の郷愁漂う、日本が世界に誇る焼肉の名店「スタミナ苑」。日本版「ザガット・サーベイ(1999)」にて、全レストラン中"第1位"を獲得。交通の便こそ良くないが、それでも行列の絶えない超人気店だ。創業以来、変わらぬ手法で丁寧に仕込まれた肉、そこに自家製タレ以外の味付けは不要だろう。小ぶりなガスロースターで焼き、一口食べれば、“並んでよかった”としみじみ思うはずだ。
-          </p>
-        </Card> */}
         {restaurants &&
           restaurants.map((restaurant) => (
             <Card key={restaurant.id} className="mb-4">
