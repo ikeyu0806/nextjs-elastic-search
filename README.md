b4db8632d583d05a5f4558ee67ae5d9cc7b5d0f8
ikeyu0806 <ikeyu0806@gmail.com>
2025-07-23 09:12:37 +0900

    run prettier

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ ElasticSearch勉強用のレポジトリです。
 Next.jsのフロントエンドからElasticSearchを操作します。
 
 サーバ起動
+
 ```
 docker compose build
 docker compose up
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
+import { Button, Checkbox, Label, TextInput } from 'flowbite-react'
 
-import { Button, Checkbox, Label, TextInput } from "flowbite-react";
-
-export  default function Home() {
+export default function Home() {
   return (
     <form className="flex max-w-md flex-col gap-4">
       <div>
         <div className="mb-2 block">
           <Label htmlFor="email1">Your email</Label>
         </div>
-        <TextInput id="email1" type="email" placeholder="name@flowbite.com" required />
+        <TextInput
+          id="email1"
+          type="email"
+          placeholder="name@flowbite.com"
+          required
+        />
       </div>
       <div>
         <div className="mb-2 block">
@@ -22,5 +26,5 @@ export  default function Home() {
       </div>
       <Button type="submit">Submit</Button>
     </form>
-  );
+  )
 }
