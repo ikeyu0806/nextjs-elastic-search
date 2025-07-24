c490b9b72e90282b4fa72f8d83a4fdfa4d7a16a5
ikeyu0806 <ikeyu0806@gmail.com>
2025-07-24 12:56:53 +0900

    implement restaurants/register/page.tsx, delete restaurant/page.tsx

--- app/restaurants/register/page.tsx
+++ app/restaurants/register/page.tsx
@@ -1,7 +1,44 @@
+import { TextInput } from "flowbite-react"
+
 export default function RegisterRestaurant() {
   return (
     <div className='p-4'>
       <h1 className='text-3xl font-bold mb-4'>レストラン登録</h1>
+      <form>
+        <div className='mb-4'>
+          <TextInput
+            id='name'
+            type='text'
+            placeholder='レストラン名'
+            required
+            className='mb-2'
+          />
+          <TextInput
+            id='category'
+            type='text'
+            placeholder='カテゴリー'
+            required
+            className='mb-2'
+          />
+          <TextInput
+            id='address'
+            type='text'
+            placeholder='住所'
+            required
+            className='mb-2'
+          />
+          <TextInput
+            id='rating'
+            type='number'
+            placeholder='評価 (1-5)'
+            required
+            className='mb-2'
+          />
+        </div>
+        <button type='submit' className='btn'>
+          登録
+        </button>
+      </form>
     </div>
   )
 }
