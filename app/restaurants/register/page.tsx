13a96472729441da5aeef50b05bb5b02a5c962f8
ikeyu0806 <ikeyu0806@gmail.com>
2025-07-24 14:58:19 +0900

    WIP implement create index form

--- app/restaurants/register/page.tsx
+++ app/restaurants/register/page.tsx
@@ -1,30 +1,79 @@
+'use client'
+
+import { useState } from 'react'
 import { TextInput, Textarea, Button } from 'flowbite-react'
 
 export default function RegisterRestaurant() {
+  const [name, setName] = useState('')
+  const [description, setDescription] = useState('')
+  const [category, setCategory] = useState('')
+  const [address, setAddress] = useState('')
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const restaurant = {
+      name,
+      description,
+      category,
+      address,
+      // tags: [], // TODO
+    }
+
+    try {
+      const res = await fetch('/api/restaurants', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify([restaurant]), // 複数登録APIに1件でも配列で送る
+      })
+
+      if (!res.ok) {
+        throw new Error('登録に失敗しました')
+      }
+
+      alert('飲食店を登録しました！')
+      setName('')
+      setDescription('')
+      setCategory('')
+      setAddress('')
+    } catch (err) {
+      alert('エラーが発生しました')
+      console.error(err)
+    }
+  }
+
   return (
     <div className='grid grid-cols-12 p-4'>
       <div className='col-span-4'></div>
       <div className='col-span-4'>
-        <h1 className='text-3xl font-bold mb-4'>レストラン登録</h1>
-        <form>
+        <h1 className='text-3xl font-bold mb-4'>飲食店登録</h1>
+        <form onSubmit={handleSubmit}>
           <div className='mb-4'>
             <TextInput
               type='text'
               id='restaurantName'
-              placeholder='レストラン名'
+              placeholder='飲食店名'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               className='mb-2'
             />
             <Textarea
               id='description'
-              placeholder='レストランの説明'
-              className='mb-2'
+              placeholder='飲食店の説明'
               rows={8}
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              className='mb-2'
             />
             <TextInput
               id='category'
               type='text'
               placeholder='カテゴリー'
               required
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
               className='mb-2'
             />
             <TextInput
@@ -32,6 +81,8 @@ export default function RegisterRestaurant() {
               type='text'
               placeholder='住所'
               required
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
               className='mb-2'
             />
           </div>
